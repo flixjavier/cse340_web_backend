d34e7b61aba9a4c63c6e4c4b339dfefa791d14f2
flixjavier <flixjavier@gmail.com>
2026-09-05 12:10:09 -0600

    EJS templates. Update server.js

--- server.js
+++ server.js
@@ -9,30 +9,42 @@ const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
 //Define the port number the server will listen on
 const PORT = process.env.PORT || 3000;
 
+// Get the current file path and directory name
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Create an instance of the Express application
 const app = express();
+// set ejs as the templating engine
+app.set('view engine', 'ejs');
+
+//tell express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+
 
 /**
  * configure Express middleware
  */
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
+
 /**
  * Routes
  */
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/views/home.html'));
+app.get('/', async(req, res) => {
+  const title = 'Home';
+  res.render('home', { title });
 });
 
-app.get('/organizations', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/views/organizations.html'));
+app.get('/organizations', async(req, res) => {
+  const title = 'Organizations';
+  res.render('organizations', { title });
 });
 
-app.get('/projects', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/views/projects.html'));
+app.get('/projects', async(req, res) => {
+  const title = 'Projects';
+  res.render('projects', { title });
 });
 
 app.listen(PORT, () => {
